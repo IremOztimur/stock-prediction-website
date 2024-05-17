045c0b919705eb379f4e9bae7a70e7943baeb9bc
IremOztimur <iremoztimur07@gmail.com>
2024-05-17 16:53:09 +0300

    [UPDATE] structured columns and design

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import pandas as pd
+import fetch
 
 START = "2018-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction Website")
+col1, col2 = st.columns(2)
 
-stocks = ("AAPL", "GOOG", "MSFT", "GME")
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
-n_years = st.slider("Years of prediction", 1, 4)
-period = n_years * 365
+with col1:
+	with st.spinner("Fetching data..."):
+		fetch.show_companies()
 
 @st.cache_data
 def load_data(ticker):
@@ -23,13 +25,24 @@ def load_data(ticker):
 	data.reset_index(inplace=True)
 	return data
 
-data_load_state = st.text("Load data...")
-data = load_data(selected_stock)
-data_load_state.text("Loading data... done")
+with col2:
+	selected_stock = st.text_input("Enter Stock Symbol", 'GOOG')
+	n_years = st.slider("Years of prediction", 1, 4)
+	period = n_years * 365
+	data = load_data(selected_stock)
+	st.divider()
+	company_info = yf.Ticker(selected_stock).info
+	st.subheader("Company Details")
+	st.write("**Company Name:**", company_info.get('longName', 'N/A'))
+	st.write("**Industry:**", company_info.get('industry', 'N/A'))
+	st.write("**Sector:**", company_info.get('sector', 'N/A'))
+	st.write("**Country:**", company_info.get('country', 'N/A'))
+	st.write("**Market Cap:**", company_info.get('marketCap', 'N/A'))
 
-st.subheader('Raw Data')
+st.subheader('Raw Data of {}'.format(selected_stock))
 st.write(data.head())
-st.write(data.tail())
+st.subheader("Summaries of {} Data".format(selected_stock))
+st.write(data.describe())
 
 def plot_raw_data():
 	fig = go.Figure()
